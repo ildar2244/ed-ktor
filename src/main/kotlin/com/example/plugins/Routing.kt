edbdc6b4d17aca6b8a7ec2d9f6b51f20da0e9195
ildar2244 <ildar2244@gmail.com>
2023-06-11 14:21:09 +0300

    Ktor: creating HTTP APIs

---
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -0,0 +1,15 @@
+package com.example.plugins
+
+import com.example.routes.*
+import io.ktor.server.routing.*
+import io.ktor.server.response.*
+import io.ktor.server.application.*
+
+fun Application.configureRouting() {
+    routing {
+        customerRouting()
+        listOrdersRoute()
+        getOrderRoute()
+        totalizeOrderRoute()
+    }
+}
